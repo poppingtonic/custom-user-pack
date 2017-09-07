002c7e43d4ddf6488f8708736cb3d39350064d0c
Brian Muhia <poppingtonic@gmail.com>
2017-09-07 11:23:39 +0300

    Update version and name

--- info.el
+++ info.el
@@ -1,3 +1,3 @@
-(live-pack-name "user-template-pack")
-(live-pack-version "0.0.1alpha")
-(live-pack-description "Your own user pack - modify and customise this to your hearts content.")
+(live-pack-name "brian-pack")
+(live-pack-version "0.0.3alpha")
+(live-pack-description "Some personally useful customizations")
